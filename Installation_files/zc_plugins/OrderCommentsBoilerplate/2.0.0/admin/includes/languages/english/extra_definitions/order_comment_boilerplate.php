a5de8a8acf0e154fd6a80b3379e8a4206fcd1c8d
Erik Kerkhoven <info@zen4all.nl>
2025-02-20 08:11:38 +0100

    Update language file structure to ZC 2.0

--- Installation_files/zc_plugins/OrderCommentsBoilerplate/2.0.0/admin/includes/languages/english/extra_definitions/order_comment_boilerplate.php
+++ Installation_files/zc_plugins/OrderCommentsBoilerplate/2.0.0/admin/includes/languages/english/extra_definitions/order_comment_boilerplate.php
@@ -2,12 +2,15 @@
 
 /*
  * @package Order Comment Boilerplate
- * @copyright Copyright 2008-2016 Zen4All
+ * @copyright Copyright 2008-202025 Zen4All
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: order_comment_boilerplate.php 1.0 2016-04-28
+ * @version $Id: order_comment_boilerplate.php 2.0 2025-02-20
  */
 
-define('BOX_TOOLS_ORDER_COMMENT_BOILERPLATE', 'Order Comment Boilerplate');
+$define = [
+  'BOX_TOOLS_ORDER_COMMENT_BOILERPLATE' => 'Order Comment Boilerplate',
+  'ENTRY_PREDEFINED_COMMENTS' => 'Predefined Comments',
+  'TEXT_SELECT_COMMENT' => '-- Please choose --',
+];
 
-define('ENTRY_PREDEFINED_COMMENTS', 'Predefined Comments');
-define('TEXT_SELECT_COMMENT', '-- Please choose --');
+return $define;
