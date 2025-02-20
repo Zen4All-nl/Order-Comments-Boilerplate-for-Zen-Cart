bbb659771f94bc202c954551440811409e8dcac4
Erik Kerkhoven <info@zen4all.nl>
2025-02-20 08:54:49 +0100

    Update manifest.php

--- Installation_files/zc_plugins/OrderCommentsBoilerplate/2.0.0/manifest.php
+++ Installation_files/zc_plugins/OrderCommentsBoilerplate/2.0.0/manifest.php
@@ -1,15 +1,13 @@
 <?php
-
 return [
-
-    'pluginType' => 'free',
-    'pluginAuthor' => 'Zen4All (design75)',
+    'pluginVersion' => 'v2.0.0',
     'pluginName' => 'Order Comments Boilerplate',
     'pluginDescription' => '',
-    'pluginVersion' => 'v2.0.0',
-    'pluginId' => 2222,
-    'managed' => true,
-    'zcVersions' => ['v157'],
+    'pluginAuthor' => 'Zen4All (design75)',
+    'pluginId' => 2222, // ID from Zen Cart forum
+    'zcVersions' => ['v157,v200,v210'],
     'pluginGroups' => [],
-    'changelog' => [],
+    'changelog' => 'https://github.com/Zen4All-nl/Order-Comments-Boilerplate-for-Zen-Cart/releases', // online URL (eg github release tag page, or changelog file there) or local filename only, ie: changelog.txt (in same dir as this manifest file)
+    'github_repo' => 'https://github.com/Zen4All-nl/Order-Comments-Boilerplate-for-Zen-Cart', // url
+    'pluginGroups' => [],
 ];
